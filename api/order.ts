f25a5926c79f0225b841c3768f508b2207aa39dd
ibrokhim-18 <ibrokhimazimboev@gmail.com>
2026-05-13 01:49:48 +0500

    Update order.ts

--- api/order.ts
+++ api/order.ts
@@ -1,23 +1,24 @@
-export default async function handler(req, res) {
-  const { service, phone, name } = req.body;
+import default async function handler(req, res) {
+    const { service, phone, name } = req.body;
 
-  const text = `📦 Новая заявка
+    const text = `Новая заявка
 Услуга: ${service}
 Имя: ${name || "без имени"}
 Телефон: ${phone}`;
 
-  const BOT_TOKEN = process.env.BOT_TOKEN;
+    const BOT_TOKEN = process.env.BOT_TOKEN || '';
+    const CHAT_ID = process.env.CHAT_ID || '';
 
-  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      chat_id: process.env.CHAT_ID,
-      text,
-    }),
-  });
-
-  res.status(200).json({ ok: true });
+    await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            chat_id: CHAT_ID,
+            text,
+        }),
+    });
+    
+    res.status(200).json({ ok: true });
 }
