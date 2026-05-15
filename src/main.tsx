756195fcb5c71b5dbbdf66496da9295ae284d9b7
ibrokhim-18 <ibrokhimazimboev@gmail.com>
2026-05-16 02:27:20 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Landing from "./Landing";
 import "./index.css";
+import "./i18n"; // ← подключение переводов
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
