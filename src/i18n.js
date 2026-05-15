2951cf02e644dcc2d07eac77d81ac22ee5a527d1
ibrokhim-18 <ibrokhimazimboev@gmail.com>
2026-05-16 02:22:06 +0500

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import ruTranslation from './locales/ru.json';
+import uzTranslation from './locales/uz.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      ru: { translation: ruTranslation },
+      uz: { translation: uzTranslation }
+    },
+    fallbackLng: 'ru',
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
