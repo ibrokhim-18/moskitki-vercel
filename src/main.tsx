5d51d637afb4495a9b1bae172423e4a7d12af744
ibrokhim-18 <ibrokhimazimboev@gmail.com>
2026-05-16 03:41:16 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga4";
 import Landing from "./Landing";
 import "./index.css";
-import "./i18n"; // ← подключение переводов
+
+// Инициализация Google Analytics с вашим ID
+ReactGA.initialize("G-GPD3KZLEFQ");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
